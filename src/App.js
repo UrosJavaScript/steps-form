524f3744d2e0ba176d1c5609250028e5089bcad8
UrosJavaScript <kowcicuros70@gmail.com>
2024-01-07 12:19:41 +0100

    add logic for step click button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,49 @@
-import React from "react";
+import { useState } from "react";
+
+const messages = [
+  "Basic Message ✨",
+  "Apply for jobs 🙌",
+  "Invest your new income 😃",
+];
 
 export default function App() {
-  return <div>App</div>;
+  const [step, setStep] = useState(1);
+
+  function handlePrevious() {
+    if (step > 1) setStep((prev) => prev - 1);
+  }
+  function handleNext() {
+    if (step < 3) setStep((prev) => prev + 1);
+  }
+
+  return (
+    <div className="container">
+      <div className="wrapper-steps">
+        <div className="numbers">
+          <div className={step >= 1 ? "active" : ""}>1</div>
+          <div className={step >= 2 ? "active" : ""}>2</div>
+          <div className={step >= 3 ? "active" : ""}>3</div>
+        </div>
+
+        <p className="message">
+          Step {step}: {messages[step - 1]}
+        </p>
+
+        <div className="buttons">
+          <button
+            className={`${step === 1 ? "disabled" : ""}`}
+            onClick={handlePrevious}
+          >
+            Previous
+          </button>
+          <button
+            className={`${step === 3 ? "disabled" : ""}`}
+            onClick={handleNext}
+          >
+            Next
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
